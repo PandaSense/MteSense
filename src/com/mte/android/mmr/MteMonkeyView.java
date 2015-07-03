200ec481cd1bbbf6dad8ae30e077d3109e51d1cd
PandaSense <d3nick@gmail.com>
2015-07-03 18:58:49 +0800

    Add MteMonkeyRunner module for Android testing

--- src/com/mte/android/mmr/MteMonkeyView.java
+++ src/com/mte/android/mmr/MteMonkeyView.java
@@ -18,59 +18,59 @@ public class MteMonkeyView {
 
     private IChimpView impl;
 
-	public MteMonkeyView(IChimpView impl) {
-		this.impl = impl;
-	}
-	
-	public boolean getChecked(){
-		return impl.getChecked();
-	}
-	
-	public String getViewClass(){
-		return impl.getViewClass();
-	}
-	
-	public String getText(){
-		return impl.getText();
-	}
-	
-	public ChimpRect getLocation(){
-		return impl.getLocation();
-	}
-	
-	public boolean getSelected(){
-		return impl.getSelected();
-	}
-	
-	public void setSelected(boolean selected){
-		 impl.setSelected(selected);
-	}
-	
-	public boolean getFocused(){
-		return impl.getFocused();
-	}
-	
-	public void setFocused(boolean focused){
-		impl.setFocused(focused);
-	}
-	
-	public MteMonkeyView getParent(){
-		MteMonkeyView parent=new MteMonkeyView(impl.getParent());
-		return parent;
-	}
-	
-	public List<IChimpView> getChildren(){		
-		List<IChimpView> chimpChildren = impl.getChildren();
-		return chimpChildren;
-	}
-	
-	
-	public AccessibilityIds getAccessibilityIds(){		
-		return impl.getAccessibilityIds();
-	}
-	
-	public boolean getEnabled(){
-		return impl.getEnabled();
-	}
-	
+    public MteMonkeyView(IChimpView impl) {
+        this.impl = impl;
+    }
+
+    public boolean getChecked(){
+        return impl.getChecked();
+    }
+
+    public String getViewClass(){
+        return impl.getViewClass();
+    }
+
+    public String getText(){
+        return impl.getText();
+    }
+
+    public ChimpRect getLocation(){
+        return impl.getLocation();
+    }
+
+    public boolean getSelected(){
+        return impl.getSelected();
+    }
+
+    public void setSelected(boolean selected){
+        impl.setSelected(selected);
+    }
+
+    public boolean getFocused(){
+        return impl.getFocused();
+    }
+
+    public void setFocused(boolean focused){
+        impl.setFocused(focused);
+    }
+
+    public MteMonkeyView getParent(){
+        MteMonkeyView parent=new MteMonkeyView(impl.getParent());
+        return parent;
+    }
+
+    public List<IChimpView> getChildren(){
+        List<IChimpView> chimpChildren = impl.getChildren();
+        return chimpChildren;
+    }
+
+
+    public AccessibilityIds getAccessibilityIds(){
+        return impl.getAccessibilityIds();
+    }
+
+    public boolean getEnabled(){
+        return impl.getEnabled();
+    }
+
 }
