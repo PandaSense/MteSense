9d989a51e961610f223109b339331dbefeb94eda
PandaSense <d3nick@gmail.com>
2015-11-14 14:42:38 +0800

    Change sample files

--- src/test/example/testcase/MteMonkeyRunnerTest.java
+++ src/test/example/testcase/MteMonkeyRunnerTest.java
@@ -8,52 +8,52 @@ import com.mte.android.mmr.MteMonkeyRunner;
 import com.mte.util.DateTimeUtil;
 
 public class MteMonkeyRunnerTest {
-	
-	public static void main(String args[]){
-		
-		String apppath="./app/android/oschina/osc-android-app-2.2.apk";
-		String packageName="net.oschina.app";
-		String startActivity="net.oschina.app/.AppStart";
-				
-		MteMonkeyDevice device=MteMonkeyRunner.waitForConnection(100000,"HC477WY00656");
-			
-		System.out.println("Device name is : " + device.getProperty("build.model"));
-		
-//        for(String prop:device.getPropertyList()){
-//
-//            System.out.println(prop +" : "+device.getProperty(prop));
-//        }
-//
-//        device.installPackage(apppath);
-//
-//        device.startActivity(startActivity);
-//
-//        MteMonkeyRunner.sleep(30000);
+
+    public static void main(String args[]) {
+
+        String apppath = "./app/android/oschina/osc-android-app-2.2.apk";
+        String packageName = "net.oschina.app";
+        String startActivity = "net.oschina.app/.AppStart";
+
+        MteMonkeyDevice device = MteMonkeyRunner.waitForConnection(100000, "HC477WY00656");
 
 
-//        MteMonkeyImage image=new MteMonkeyImage(device.takeSnapshot());
-//        
-//        image.writeToFile("./screen/MteMonkeyRunnerTest"+DateTimeUtil.getCurrentDateTime()+".png", "png");
-//        
-//        MteMonkeyRunner.sleep(10000);
+        System.out.println("Device name is : " + device.getProperty("build.model"));
 
+        for (String prop : device.getPropertyList()) {
 
-//        Collection<String> viewLst=device.getViewIdList();
-//
-//        System.out.println("device.getViewIdList() is : "+viewLst.size());
-//
-//        if(viewLst.size()>=1){
-//            for(String prop:viewLst){
-//                System.out.println(prop);
-//            }
-//        }
-        
-//        MteMonkeyRunner.sleep(20000);
-//
-//        device.removePackage(packageName);
+            System.out.println(prop + " : " + device.getProperty(prop));
+        }
+
+        device.installPackage(apppath);
+
+        device.startActivity(startActivity);
+
+        MteMonkeyRunner.sleep(30000);
+
+        MteMonkeyImage image = new MteMonkeyImage(device.takeSnapshot());
+
+        image.writeToFile("./screen/MteMonkeyRunnerTest" + DateTimeUtil.getCurrentDateTime() + ".png", "png");
+
+        MteMonkeyRunner.sleep(10000);
+
+        Collection<String> viewLst = device.getViewIdList();
+
+        System.out.println("device.getViewIdList() is : " + viewLst.size());
+
+        if (viewLst.size() >= 1) {
+            for (String prop : viewLst) {
+                System.out.println(prop);
+            }
+        }
+// This is test
+
+        MteMonkeyRunner.sleep(20000);
+
+        device.removePackage(packageName);
 
         device.dispose();
 
-	}
+    }
 
 }
--- src/test/example/testcase/MteSenseSeleniumTestCase.java
+++ src/test/example/testcase/MteSenseSeleniumTestCase.java
@@ -1,6 +1,7 @@
 package test.example.testcase;
 
 import com.mte.common.base.MteSenseBaseCase;
+import com.mte.util.ReportUtil;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
@@ -15,18 +16,21 @@ public class MteSenseSeleniumTestCase extends MteSenseBaseCase {
 
     private Logger logger = Logger.getLogger(MteSenseSeleniumTestCase.class);
     private String testCaseName = this.getClass().getSimpleName();
+    private ReportUtil report = new ReportUtil(testCaseName,props.get("mte.reportFile.path"));
 
     @Before
     public void setUp() throws Exception {
         beforeClass("chrome");
         asBaseCore.setTestCaseName(testCaseName);
+        this.setReporter(report);
     }
 
     @Test
-    public void testMain() throws Exception{
+    public void testMain() throws Exception {
 
         asBaseCore.pause(4000);
-
+        report.printStep("this is test",1);
+        report.printImageIntoReport(asBaseCore.getCaptureScreenshotFile());
 
     }
 
